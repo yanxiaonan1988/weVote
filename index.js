b6925f4e9145c54ec186bf4d7393e75e0a9efc53
yanxiaonan1988 <yanxiaonan1988@gmail.com>
2018-11-07 09:49:17 +0800

    basic create vote demo

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(fileUpload());
 
-app.get('/api', (req, res) => res.send('Hello World!'));
+app.get('/api', (req, res, next) => res.send('Hello World!'));
 
+
+
+app.post('/api/vote/create', (req, res, next) => {
+    let voteFile = req.files.voteFile;
+    console.log(req.body.voteTitle);
+
+    voteFile.mv('data/xxx.jpg', function(err) {
+        res.redirect('/');
+    });
+});
 
 
 
